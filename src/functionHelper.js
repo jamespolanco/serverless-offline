e9f5969b1c518471a8b276ef1b3f7f833ca86dcf
James Polanco <jpolanco@foreseemed.com>
2018-01-19 14:44:16 -0800

    Adding logging in of lost/parsed statements
    When we split based on the regex, we need to log an other statement

--- src/functionHelper.js
+++ src/functionHelper.js
@@ -2,41 +2,52 @@
 
 const debugLog = require('./debugLog');
 
-function runPythonHandler(funOptions, options){
-    var spawn = require("child_process").spawn;
-      return function(event,context){
-      var process = spawn('sls',["invoke", "local", "-f", funOptions.funName],
-          {stdio: ['pipe', 'pipe', 'pipe'], shell: true, cwd:funOptions.servicePath});
-      process.stdin.write(JSON.stringify(event)+"\n");
-      process.stdin.end();
-      let results = ''
-      process.stdout.on('data', (data) => {
-         results = results + data;
-      });
-      process.stderr.on('data', (data) => {
-         context.fail(data);
-      });
-      process.on('close', (code) => {
-          if (code == 0) {
-              try {
-                const resultsData = results.match(/^{(.|\n)*?}$/gm).pop();
-                context.succeed( JSON.parse(resultsData) );
-              } catch (ex) {
-                context.fail(results);
-              }
-          } else {
-              context.succeed( code ,results );
-          }  
-        });
+function runPythonHandler(funOptions, options) {
+  const spawn = require('child_process').spawn;
+  
+  return function (event, context) {
+    const process = spawn('sls', ['invoke', 'local', '-f', funOptions.funName], {
+      stdio: ['pipe', 'pipe', 'pipe'],
+      shell: true,
+      cwd: funOptions.servicePath,
+    });
+    process.stdin.write(`${JSON.stringify(event)}\n`);
+    process.stdin.end();
+    let results = '';
+    process.stdout.on('data', data => {
+      results += data;
+    });
+    process.stderr.on('data', data => {
+      context.fail(data);
+    });
+    process.on('close', code => {
+      if (code == 0) {
+        try {
+          const resultMatch = results.match(/^{(.|\n)*?}$/gm);
+          const resultJson = resultMatch.pop();
+          console.log(resultMatch.join(''));
+          context.succeed(JSON.parse(resultJson));
+        }
+        catch (ex) {
+          context.fail(results);
+        }
       }
+      else {
+        context.succeed(code, results);
+      }
+    });
+  };
 }
 
 module.exports = {
-  getFunctionOptions(fun, funName, servicePath,serviceRuntime) {
-    console.log(fun, funName, servicePath)
+  getFunctionOptions(fun, funName, servicePath, serviceRuntime) {
+    console.log(fun, funName, servicePath);
     // Split handler into method name and path i.e. handler.run
     const handlerFile = fun.handler.split('.')[0];
-    const handlerName = fun.handler.split('/').pop().split('.')[1];
+    const handlerName = fun.handler
+      .split('/')
+      .pop()
+      .split('.')[1];
 
     return {
       funName,
@@ -50,7 +61,6 @@ module.exports = {
     };
   },
 
-  
   // Create a function handler
   // The function handler is used to simulate Lambda functions
   createHandler(funOptions, options) {
@@ -63,16 +73,23 @@ module.exports = {
         if (!key.match('node_modules')) delete require.cache[key];
       }
     }
-    let user_python = true
+    const user_python = true;
     let handler = null;
-    if (['python2.7', 'python3.6'].indexOf(funOptions['serviceRuntime']) !== -1){
-      handler = runPythonHandler(funOptions, options)
-    } else {
+    if (
+      ['python2.7', 'python3.6'].indexOf(funOptions.serviceRuntime) !== -1
+    ) {
+      handler = runPythonHandler(funOptions, options);
+    }
+    else {
       debugLog(`Loading handler... (${funOptions.handlerPath})`);
       handler = require(funOptions.handlerPath)[funOptions.handlerName];
     }
     if (typeof handler !== 'function') {
-      throw new Error(`Serverless-offline: handler for '${funOptions.funName}' is not a function`);
+      throw new Error(
+        `Serverless-offline: handler for '${
+          funOptions.funName
+        }' is not a function`
+      );
     }
 
     return handler;
